fc8e32165c304822159e36eb137c3c79120b0479
hasini-kothamasu <hasinikkvl@gmail.com>
2025-12-23 13:10:34 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,10 @@
-// app/firebaseConfig.js
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import {
+  initializeAuth,
+  getReactNativePersistence
+} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAfwlGnvfokT7jvCbI9pblYx-LfhjnemCU",
@@ -10,11 +13,13 @@ const firebaseConfig = {
   storageBucket: "servies-70035.firebasestorage.app",
   messagingSenderId: "903718976224",
   appId: "1:903718976224:web:5f19074e590b4adaf4ae85",
-  measurementId: "G-RJ2J20QF50"
 };
 
-let app;
-try { app = initializeApp(firebaseConfig); } catch (e) { /* ignore hot reload */ }
+const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
 export const db = getFirestore(app);
+
